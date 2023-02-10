e65f260e63b5df98b1a847fdc74aad88f2ddea01
MHines10 <cd.mhines@gmail.com>
2023-02-10 06:42:43 -0500

    first commit

---
+++ app/blueprints/api/routes.py
@@ -0,0 +1,68 @@
+from flask import request
+from . import api
+from .auth import basic_auth, token_auth
+from app.models import User
+
+@api.route('/token', methods=['GET'])
+@basic_auth.login_required
+def index():
+    user = basic_auth.current_user()
+    token = user.get_token()
+    return {'token': token, 'token_expiration': user.token_expiration}
+
+
+
+# Endpoint to get a single user by id
+@api.route('/users/<int:user_id>')
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return user.to_dict()
+
+# Endpoint to create a new user
+@api.route('/users', methods=['POST'])
+def create_user():
+    # Check to see that the request sent a request body that is JSON
+    if not request.is_json:
+        return {'error': 'Your request content-type must be application/json'}, 400
+    data = request.json
+    for field in ['username', 'email', 'password']:
+        if field not in data:
+            # If the field is not in the request body, throw an error saying they are missing that field
+            return {'error': f"{field} must be in request body"}, 400
+    # pull individual values from the request body
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    # Check to see if there is a User with that username and/or email
+    existing_user = User.query.filter((User.username == username)|(User.email == email)).all()
+    if existing_user:
+        return {'error': 'User with this username and/or email already exists'}, 400
+
+    # Create a new instance of User
+    new_user = User(username=username, email=email, password=password)
+    # Send back the new user info
+    return new_user.to_dict(), 201
+
+    # Update a user by id 
+@api.route('/users/<int:id>', methods=['PUT'])
+@token_auth.login_required
+def updated_user(id):
+    current_user = token_auth.current_user()
+    if current_user.id != id:
+        return {'error': 'You do not have access to update this user'}, 403
+    user = User.query.get_or_404(id)
+    data = request.json
+    user.update(data)
+    return user.to_dict()
+
+# Delete a user by id
+@api.route('/users/<int:id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_user(id):
+    current_user = token_auth.current_user()
+    if current_user.id != id:
+        return {'error': 'You do not have access to delete this user'}, 403
+    user_to_delete = User.query.get_or_404(id)
+    user_to_delete.delete()
+    return {'success': f'{user_to_delete.username} has been deleted'}
---
+++ app/models.py
@@ -0,0 +1,56 @@
+import base64
+import os
+from datetime import datetime, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs['password'])
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<User {self.id} | {self.username}>"
+
+    def check_password(self, password_guess):
+        return check_password_hash(self.password, password_guess)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'username': self.username,
+            'date_created': self.date_created,
+            # 'posts': [p.to_dict() for p in self.posts.all()]
+        }
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.commit()
+        return self.token
+
+    def revoke_token(self):
+        now = datetime.utcnow()
+        self.token_expiration = now - timedelta(seconds=1)
+        db.session.commit()
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
